57b4f22d059db263c1d0f49fdb6ff5abb22daea6
Joshua Jackson <JJackson9@student.fullsail.edu>
2023-11-10 07:56:17 -0500

    Update

--- UniverseEventHandler.cs
+++ UniverseEventHandler.cs
@@ -1,10 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: GOLNET001.UniverseEventHandler
-// Assembly: GOLNET001, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: E76B15F6-1E09-484F-8596-AD5A8957CA7D
-// Assembly location: C:\Users\joshj\Downloads\GOLDEMO003\GOLDEMO003.exe
-
-namespace GOLNET001
+﻿namespace GOLNET001
 {
   public delegate void UniverseEventHandler(object sender, UniverseEventArgs e);
 }
